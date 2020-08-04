ae3efbfede10994a6c53a079151dc2d02e0aa5a3
LIANGFR2 <372334264@qq.com>
2020-08-04 20:51:45 +0800

    [todo-list][front, olivia] feature: use BasicRoute to replace TodoContainer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import TodoContainer from './components/TodoContainer'
-
+// import TodoContainer from './components/TodoContainer'
+import BasicRoute from './components/Router'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoContainer />
+        {/* <TodoContainer /> */}
+        <BasicRoute />
       </header>
     </div>
   );
