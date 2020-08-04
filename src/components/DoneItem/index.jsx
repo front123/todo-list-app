1d9ea1cec64f343cccd1989f60b53172f56c198a
LIANGFR2 <372334264@qq.com>
2020-08-04 20:49:41 +0800

    [todo-list][front, olivia] feature: create DoneItem component

---
+++ src/components/DoneItem/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class DoneItem extends React.Component{
+    
+    render(){
+        return(
+            <div>
+                <h1>Done List</h1>
+                <a href="#/" style={{color:"white"}}>Go Back</a>
+            </div>
+        )
+    }
+}
+
+export default DoneItem;
