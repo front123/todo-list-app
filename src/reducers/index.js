a9031176aa9afce53094ac3e47950670c7833f10
LIANGFR2 <372334264@qq.com>
2020-08-04 19:56:15 +0800

    [todo-list][front, olivia] feature: add SET-DONE action in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 export default (state = {itemsList:[]}, action) => {
-    switch (action.type) {
+    switch (action.type) { 
       case 'ADD_ITEM':
         state.itemsList.push(action.item);
         return {
@@ -9,7 +9,14 @@ export default (state = {itemsList:[]}, action) => {
         return {
           itemsList: state.itemsList.filter((item, index) => action.index !== index)
         };
+      case 'SET_DONE':
+        state.itemsList[action.index].isDone = !state.itemsList[action.index].isDone
+        return {
+          itemsList: state.itemsList.slice()
+        };
       default:
         return state
     }
   }
+
+  // item:{text:'', isDone:boolean}
