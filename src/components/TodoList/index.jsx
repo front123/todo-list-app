e846a302cb45963d34d428b8bb0d5c7fb831d253
LIANGFR2 <372334264@qq.com>
2020-08-04 19:57:32 +0800

    [todo-list][front, olivia] feature: extract TodoList.propTypes

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import TodoItem from '../TodoItem';
+import PropTypes from 'prop-types';
 
-class TodoList extends React.Component{
-    
-    render(){
-        return(
+class TodoList extends React.Component {
+
+    render() {
+        return (
             <div>
                 {
-                    this.props.itemsList.map((item, index) => <TodoItem key={index} index={index} text={item.text}/>)
+                    this.props.itemsList.map((item, index) => <TodoItem key={index} index={index} text={item.text} isDone={item.isDone} />)
                 }
             </div>
         )
     }
-
-    
 }
 
 const mapStateToProps = state => {
-    return {itemsList:state.itemsList};
+    return { itemsList: state.itemsList };
 };
-
+TodoList.propTypes = {
+    itemsList: PropTypes.array
+}
 export default connect(mapStateToProps)(TodoList);
