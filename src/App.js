7a03b6884efac5d54aa8a41fd76a24439267b539
LIANGFR2 <372334264@qq.com>
2020-08-06 19:15:42 +0800

    [todo-app][front, olivia] feature: change App from function to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
-// import TodoContainer from './components/TodoContainer'
 import BasicRoute from './components/Router'
+import { connect } from 'react-redux';
+import { Spin } from 'antd';
+import { Link, HashRouter } from 'react-router-dom';
+import { Layout } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <TodoContainer /> */}
-        <BasicRoute />
-      </header>
-    </div>
-  );
+const { Header } = Layout;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <Header>
+            <Spin size="large" spinning={this.props.loading} style={{ position: "absolute", left: "50%", top: "50%" }} />
+            <HashRouter>
+              <Link to="/" >
+                <h1 className="my-todo-list" style={{ display: "inline", cursor: "pointer", fontSize: 24 }}>My Todo List </h1>
+                <span style={{ color: "white", fontSize: 24 }}>|</span>
+              </Link>
+              <Link to="/done" >
+                <h1 className="finish-text" style={{ display: "inline", fontSize: 24 }}>Finished</h1>
+              </Link>
+            </HashRouter>
+            <BasicRoute />
+          </Header>
+        </Layout>
+      </div>
+    );
+  }
 }
-
-export default App;
+const mapStateToProps = state => {
+  return { loading: state.loadingReducer }
+}
+export default connect(mapStateToProps)(App)
