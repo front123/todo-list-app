e8e81d96141919065e8c53ba9ef7cb052c8b2971
LIANGFR2 <372334264@qq.com>
2020-08-06 19:25:03 +0800

    [todo-app][front, olivia] feature:import antd.css

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers'
-
-const store = createStore(reducer);
+import 'antd/dist/antd.css';
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
