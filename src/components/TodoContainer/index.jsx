7724f1c6abf131ba5ce6efd5ce67e958b0b42139
LIANGFR2 <372334264@qq.com>
2020-08-06 19:27:22 +0800

    [todo-app][front, olivia] refactor: remove header tag to app.js

--- src/components/TodoContainer/index.jsx
+++ src/components/TodoContainer/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-// import { connect } from 'react-redux';
 import InputItem from '../InputItem';
 import TodoList from '../TodoList';
 
-class TodoContainer extends React.Component{
-    
-    render(){
-        return(
+class TodoContainer extends React.Component {
+    render() {
+        return (
             <div>
-                <h1 style={{display:"inline"}}>My Todo List</h1> 
-                <a href="#/done" style={{color:"white"}}>Finished</a> 
                 <InputItem />
                 <TodoList />
             </div>
